77bbbc4a44e7d3961df07fdf10b684f92a499fe2
Lance Endres <lanceendres@gmail.com>
2023-07-30 18:56:31 -0500

    Change how windows are saved and restored.

--- src/Library/UI/ContentPages/DigitalProductionMainPage.xaml.cs
+++ src/Library/UI/ContentPages/DigitalProductionMainPage.xaml.cs
@@ -17,7 +17,6 @@ public partial class DigitalProductionMainPage : ContentPage
 	public DigitalProductionMainPage()
 	{
 		InitializeComponent();
-
 		this.Loaded += this.OnLoaded;
 		
 	}
@@ -35,17 +34,6 @@ public partial class DigitalProductionMainPage : ContentPage
 		{
 			case OverlappedPresenter overLappedPresenter:
 				Window parentWindow = GetParentWindow();
-				RestoreWindowPosition(parentWindow, "MainWindow");
-
-				OverlappedPresenterState state = GetWindowState("MainWindow");
-				if (state == OverlappedPresenterState.Maximized)
-				{
-					overLappedPresenter.Maximize();
-				}
-				else
-				{
-					overLappedPresenter.Restore();
-				}
 
 				// Add the event handler only after setting the initial position.  Otherwise, it will overwrite the
 				// saved values and we will not get restoration of the correct position.
@@ -59,11 +47,11 @@ public partial class DigitalProductionMainPage : ContentPage
 	private void OnSizeChanged(object sender, EventArgs eventArgs)
 	{
 #if WINDOWS
-		AppWindow appWindow = this.GetAppWindow();
-		if (appWindow == null)
-		{
-			return;
-		}
+		AppWindow appWindow = GetAppWindow();
+		//if (appWindow == null)
+		//{
+		//	return;
+		//}
 
 		switch (appWindow.Presenter)
 		{
@@ -76,14 +64,6 @@ public partial class DigitalProductionMainPage : ContentPage
 	}
 
 #if WINDOWS
-	private void RestoreWindowPosition(Window window, string name)
-	{
-		window.X        = Preferences.Default.Get(name+".Position.X", window.X);
-		window.Y        = Preferences.Default.Get(name+".Position.Y", window.Y);
-		window.Width    = Preferences.Default.Get(name+".Position.Width", window.Width);
-		window.Height   = Preferences.Default.Get(name+".Position.Height", window.Height);
-	}
-
 	private void SaveWindowState(Window window, string name, OverlappedPresenterState state)
 	{
 		Preferences.Default.Set(name+".Position.State", (int)state);
@@ -99,23 +79,10 @@ public partial class DigitalProductionMainPage : ContentPage
 		}
 	}
 
-	private OverlappedPresenterState GetWindowState(string name)
-	{
-		int state = Preferences.Default.Get(name+".Position.State", (int)OverlappedPresenterState.Maximized);
-		return (OverlappedPresenterState)state;
-	}
-
     protected AppWindow GetAppWindow()
     {
 		MauiWinUIWindow mauiWinUIWindow = GetParentWindow().Handler.PlatformView as MauiWinUIWindow;
-		if (mauiWinUIWindow == null)
-		{
-			return null;
-		}
-        var handle = WinRT.Interop.WindowNative.GetWindowHandle(mauiWinUIWindow);
-        var id = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(handle);
-        var appWindow = AppWindow.GetFromWindowId(id);
-        return appWindow;
+		return DigitalProduction.UI.AppTools.GetAppWindow(mauiWinUIWindow);
     }
 #endif
 
