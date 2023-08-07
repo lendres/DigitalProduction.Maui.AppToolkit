b5619d5a97ba344cd371e31ef03e1d0a7d636022
Lance Endres <lanceendres@gmail.com>
2023-08-06 20:06:01 -0500

    Add exit command

--- src/Library/UI/ContentPages/DigitalProductionMainPage.xaml.cs
+++ src/Library/UI/ContentPages/DigitalProductionMainPage.xaml.cs
@@ -2,6 +2,8 @@
 using Microsoft.Maui.Platform;
 using Microsoft.Maui.Storage;
 using Microsoft.Maui.Controls;
+using System.Windows.Input;
+
 #if WINDOWS
 using Microsoft.UI.Windowing;
 #endif
@@ -17,12 +19,24 @@ public partial class DigitalProductionMainPage : ContentPage
 	public DigitalProductionMainPage()
 	{
 		InitializeComponent();
-		this.Loaded += this.OnLoaded;
-		
 	}
 
-	private void OnLoaded(object sender, EventArgs eventArgs)
+	#endregion
+
+	#region Properties
+
+	#endregion
+
+	#region Methods
+
+	/// <summary>
+	/// Exit command.
+	/// </summary>
+	/// <param name="sender">Sender.</param>
+	/// <param name="eventArgs">Event arguments.</param>
+	protected void OnExit(object sender, EventArgs eventArgs)
 	{
+		Application.Current.Quit();
 	}
 
 	#endregion
