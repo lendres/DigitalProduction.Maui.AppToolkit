64f4d6aa9c23aaf64a1ccbe390e5976afc683844
lendres <lanceendres@gmail.com>
2024-04-15 20:08:35 -0500

    Add converters.

---
+++ src/Library/Converters/FalseIfNullConverter.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace DigitalProduction.Converters;
+
+public class FalseIfNullConverter : Microsoft.Maui.Controls.IValueConverter
+{
+	public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+	{
+		return value is null ? false : true;
+	}
+
+	public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+	{
+		throw new NotImplementedException();
+	}
+}
---
+++ src/Library/Converters/TrueIfNullConverter.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace DigitalProduction.Converters;
+
+public class TrueIfNullConverter : Microsoft.Maui.Controls.IValueConverter
+{
+	public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+	{
+		return value is null ? true : false;
+	}
+
+	public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+	{
+		throw new NotImplementedException();
+	}
+}
