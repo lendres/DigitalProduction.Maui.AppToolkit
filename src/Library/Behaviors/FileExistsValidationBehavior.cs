af1af9477d3507f489971a50617181db65c9649e
lendres <lanceendres@gmail.com>
2024-04-18 16:07:25 -0500

    Add file and directory validation behaviors, remove null converters.

---
+++ src/Library/Behaviors/FileExistsValidationBehavior.cs
@@ -0,0 +1,48 @@
+﻿using System.Globalization;
+using CommunityToolkit.Maui.Behaviors;
+using DigitalProduction.Behaviors;
+
+namespace DigitalProduction.Maui.Behaviors;
+
+/// <summary>
+/// The <see cref="FileExistsValidationBehavior"/> is a behavior that allows the user to determine if a file exists. For example, an <see cref="Entry"/> control can be styled differently depending on whether a valid or an invalid numeric input is provided. Additional properties handling validation are inherited from <see cref="ValidationBehavior"/>.
+/// </summary>
+public class FileExistsValidationBehavior : ValidationBehavior<string>
+{
+	/// <summary>
+	/// Backing BindableProperty for the <see cref="FileErroMessageConverter"/> property.
+	/// </summary>
+	public static readonly BindableProperty ErrorMessageProperty =
+		BindableProperty.Create(nameof(Error), typeof(FileErrorType), typeof(FileExistsValidationBehavior), FileErrorType.None, propertyChanged: OnValidationPropertyChanged);
+
+	/// <summary>
+	/// The minimum numeric value that will be allowed. This is a bindable property.
+	/// </summary>
+	public FileErrorType Error
+	{
+		get => (FileErrorType)GetValue(ErrorMessageProperty);
+		set => SetValue(ErrorMessageProperty, value);
+	}
+
+	/// <inheritdoc/>
+	protected override string? Decorate(string? value)
+		=> base.Decorate(value)?.Trim();
+
+	/// <inheritdoc/>
+	protected override ValueTask<bool> ValidateAsync(string? value, CancellationToken token)
+	{
+		ArgumentNullException.ThrowIfNull(value);
+
+		bool passed = File.Exists(value);
+		if (passed)
+		{
+			Error = FileErrorType.None;
+		}
+		else
+		{
+			Error = FileErrorType.FileNotFound;
+		}
+
+		return new ValueTask<bool>(passed);
+	}
+}
---
+++ src/Library/Converters/EnumToMessageConverter.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+using CommunityToolkit.Maui.Converters;
+using DigitalProduction.Reflection;
+
+namespace DigitalProduction.Maui.Converters;
+
+/// <summary>
+///     Converts an <see cref="Enum" /> to its underlying <see cref="int" /> value.
+/// </summary>
+public class EnumToMessageConverter : BaseConverterOneWay<Enum, string, Type>
+{
+	/// <inheritdoc/>
+	public override string DefaultConvertReturnValue { get; set; } = "";
+
+	enum DefaultEnum { Value }
+
+	/// <summary>
+	/// Convert a default <see cref="Enum"/> (i.e., extending <see cref="int"/>) to corresponding underlying <see cref="int"/>
+	/// </summary>
+	/// <param name="value"><see cref="Enum"/> value to convert</param>
+	/// <param name="parameter"></param>
+	/// <param name="culture">Unused: Culture to use in the converter</param>
+	/// <returns>The underlying <see cref="int"/> value of the passed enum value</returns>
+	/// <exception cref="ArgumentException">If value is not an enumeration type</exception>
+	public override string ConvertFrom(Enum value, Type? parameter = null, CultureInfo? culture = null)
+	{
+		ArgumentNullException.ThrowIfNull(value);
+
+		string description = "";
+
+		MessageAttribute? attribute = DigitalProduction.Reflection.Attributes.GetAttribute<MessageAttribute>(value);
+		if (attribute != null)
+		{
+			description = attribute.Message;
+		}
+		return description;
+	}
+}
