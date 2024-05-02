e8f7fce4d572cd0ad6e81371cf4e8edf88924de7
Lance A. Endres <lanceendres@gmail.com>
2024-05-02 11:13:53 -0500

    Update EnumToDescriptionConverter.cs

--- src/Library/Converters/EnumToDescriptionConverter.cs
+++ src/Library/Converters/EnumToDescriptionConverter.cs
@@ -4,36 +4,29 @@ using System.Globalization;
 
 namespace DigitalProduction.Converters;
 
-public class EnumToDescriptionConverter<TEnum> : BaseConverter<TEnum, string> where TEnum : struct
+public class EnumToDescriptionConverter<TEnum> : IValueConverter where TEnum : struct
 {
-	/// <inheritdoc/>
-	public override string DefaultConvertReturnValue { get; set; } = "";
-
-	/// <inheritdoc/>
-	public override TEnum DefaultConvertBackReturnValue { get; set; } = default;
-
-	/// <summary>
-	/// Converts <see cref="string"/> to an enumeration.
-	/// </summary>
-	/// <param name="value">The value to convert.</param>
-	/// <param name="culture">The culture to use in the converter.  This is not implemented.</param>
-	/// <returns>The enumeration whose DescriptionAttribute matches the specified string.</returns>
-	public override string ConvertFrom(TEnum value, CultureInfo? culture = null)
+	public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
 	{
-		string result = Reflection.Attributes.GetDescription(value);
+		if (value is null)
+		{
+			return default(TEnum);
+		}
+
+		InvalidTypeException.ThrowNotType(typeof(TEnum), value);
+		string result = DigitalProduction.Reflection.Attributes.GetDescription((TEnum)value);
 		StringIsNullOrEmptyException.ThrowIfNullOrEmpty("The enumeration must have a Description attribute.", result);
 		return result;
 	}
 
-	/// <summary>
-	/// Converts back object to <see cref="string"/>.
-	/// </summary>
-	/// <param name="value">The value to convert.</param>
-	/// <param name="culture">The culture to use in the converter.  This is not implemented.</param>
-	/// <returns>The text in the DescriptionAttribute of the enumeration.</returns>
-	public override TEnum ConvertBackTo(string value, CultureInfo? culture = null)
+	public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
 	{
-		TEnum result = Reflection.Enumerations.GetInstanceFromDescription<TEnum>(value);
-		return result;
+		if (value is null)
+		{
+			return "";
+		}
+
+		InvalidTypeException.ThrowNotType(typeof(string), value);
+		return DigitalProduction.Reflection.Enumerations.GetInstanceFromDescription<TEnum>((string)value);
 	}
 }
