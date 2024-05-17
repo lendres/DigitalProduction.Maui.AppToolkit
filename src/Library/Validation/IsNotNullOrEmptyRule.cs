b7708010bd401e5be58c4fb514a8c867fa32d49c
lendres <lanceendres@gmail.com>
2024-05-17 08:53:07 -0500

    Validation of Field entry/edit

--- src/Library/Validation/IsNotNullOrEmptyRule.cs
+++ src/Library/Validation/IsNotNullOrEmptyRule.cs
@@ -3,15 +3,14 @@
 /// <summary>
 /// Is not null or empty validation rule for string.
 /// </summary>
-/// <typeparam name="T">Type of object to validate.</typeparam>
 /// <remarks>
 /// Based on:
 /// https://github.com/dotnet-architecture/eshop-mobile-client
 /// Copyright (c) 2020 .NET Application Architecture - Reference Apps
 /// </remarks>
-public class IsNotNullOrEmptyRule<T> : ValidationRuleBase<T>
+public class IsNotNullOrEmptyRule : ValidationRuleBase<string>
 {
-    public override bool Check(T? value) =>
-        value is string str &&
-        !string.IsNullOrWhiteSpace(str);
+    public override bool Check(string? value) =>
+        value is not null &&
+        !string.IsNullOrWhiteSpace(value);
 }
