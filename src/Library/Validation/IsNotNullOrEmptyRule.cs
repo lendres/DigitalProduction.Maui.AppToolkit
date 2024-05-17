d090943bf1a8ce0730461360298e1f91a00db88a
lendres <lanceendres@gmail.com>
2024-05-17 06:26:13 -0500

    Validation initial commit

---
+++ src/Library/Validation/IsNotNullOrEmptyRule.cs
@@ -0,0 +1,17 @@
+﻿namespace DigitalProduction.Validation;
+
+/// <summary>
+/// Is not null or empty validation rule for string.
+/// </summary>
+/// <typeparam name="T">Type of object to validate.</typeparam>
+/// <remarks>
+/// Based on:
+/// https://github.com/dotnet-architecture/eshop-mobile-client
+/// Copyright (c) 2020 .NET Application Architecture - Reference Apps
+/// </remarks>
+public class IsNotNullOrEmptyRule<T> : ValidationRuleBase<T>
+{
+    public override bool Check(T? value) =>
+        value is string str &&
+        !string.IsNullOrWhiteSpace(str);
+}
