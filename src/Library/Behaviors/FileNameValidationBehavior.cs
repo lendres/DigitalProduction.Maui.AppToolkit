48c80eb2fe85127d078f6f6d1453440014181577
lendres <lanceendres@gmail.com>
2024-05-21 06:59:36 -0500

    Add file name validation behavior.

---
+++ src/Library/Behaviors/FileNameValidationBehavior.cs
@@ -0,0 +1,43 @@
+﻿using CommunityToolkit.Maui.Behaviors;
+using DigitalProduction.IO;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DigitalProduction.Behaviors;
+
+/// <summary>
+/// The <see cref="FileNameValidationBehavior"/> is a behavior that allows the user to determine if a file exists.  Additional properties handling validation are inherited from <see cref="ValidationBehavior"/>.
+/// </summary>
+public class FileNameValidationBehavior : ErrorReportableValidationBehavior<string, ValidFileNameResult>
+{
+	/// <summary>
+	/// Backing BindableProperty for the <see cref="RequireFileToExist"/> property.
+	/// </summary>
+	public static readonly BindableProperty RequireFileToExistProperty =
+		BindableProperty.Create(nameof(RequireFileToExist), typeof(bool), typeof(DirectoryExistsValidationBehavior), default(bool), propertyChanged: OnValidationPropertyChanged);
+
+	/// <summary>
+	/// The associated error found during the validation. This is a bindable property.  It is recommended that the TError type include a "None" error code.
+	/// </summary>
+	public bool RequireFileToExist
+	{
+		get => (bool)GetValue(RequireFileToExistProperty);
+		set => SetValue(RequireFileToExistProperty, value);
+	}
+
+	/// <inheritdoc/>
+	protected override ValueTask<bool> ValidateAsync(string? value, CancellationToken token)
+	{
+		ArgumentNullException.ThrowIfNull(value);
+
+		if (RequireFileToExist)
+		{
+			Error = DigitalProduction.IO.Path.IsValidFileName(value, new ValidFileNameOptions() { RequirePathToExist = true });
+		}
+		else
+		{
+			Error = DigitalProduction.IO.Path.IsValidFileName(value);
+		}
+
+		return new ValueTask<bool>(Error == ValidFileNameResult.Valid);
+	}
+}
