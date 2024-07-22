bd404f1a1c24c27a7f8665bfcb8c42d0d7e21b37
lendres <lanceendres@gmail.com>
2024-07-21 20:20:16 -0500

    Add a base Popup view.

---
+++ src/Library/UI/Views/PopupView.xaml.cs
@@ -0,0 +1,38 @@
+﻿using CommunityToolkit.Maui.Views;
+
+namespace DigitalProduction.UI;
+
+public partial class PopupView : Popup
+{
+	#region Construction
+
+	public PopupView()
+	{
+	}
+
+	#endregion
+
+	#region Events
+
+	protected void OnLoadedSetFocus(object? sender, EventArgs eventArgs)
+	{
+		if (sender is VisualElement control)
+		{
+			control.Focus();
+		}
+	}
+
+	async protected virtual void OnSaveButtonClicked(object? sender, EventArgs eventArgs)
+	{
+		CancellationTokenSource cancelationTokenSource = new(TimeSpan.FromSeconds(5));
+		await CloseAsync(true, cancelationTokenSource.Token);
+	}
+
+	async protected void OnCancelButtonClicked(object? sender, EventArgs eventArgs)
+	{
+		CancellationTokenSource cancelationTokenSource = new(TimeSpan.FromSeconds(5));
+		await CloseAsync(false, cancelationTokenSource.Token);
+	}
+
+	#endregion
+}
