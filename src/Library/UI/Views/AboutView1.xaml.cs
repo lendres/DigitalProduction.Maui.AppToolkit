cd5c4565243eb79b2f005bf76bdafbd33ff9092e
lendres <lanceendres@gmail.com>
2024-07-26 18:04:05 -0500

    Add about dialog

---
+++ src/Library/UI/Views/AboutView1.xaml.cs
@@ -0,0 +1,21 @@
+﻿using DigitalProduction.ViewModels;
+
+namespace DigitalProduction.UI;
+
+public partial class AboutView1 : PopupView
+{
+	#region Construction
+
+	public AboutView1(AboutViewModel viewModel)
+	{
+		BindingContext = viewModel;
+	}
+
+	#endregion
+
+	#region Properties
+	#endregion
+
+	#region Events
+	#endregion
+}
