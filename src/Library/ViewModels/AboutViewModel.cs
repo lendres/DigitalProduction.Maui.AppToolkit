9ed40f7a0d6a73f62d71afd8d9d2772fea55f3c9
lendres <lanceendres@gmail.com>
2024-07-27 08:37:22 -0500

    Create AboutViewModel.cs

---
+++ src/Library/ViewModels/AboutViewModel.cs
@@ -0,0 +1,39 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Data.Translation.Validation;
+using DigitalProduction.Validation;
+
+namespace DigitalProduction.ViewModels;
+
+public partial class AboutViewModel : ObservableObject
+{
+	#region Fields
+
+	[ObservableProperty]
+	private string				_title					= "";
+	
+	[ObservableProperty]
+	private string				_authors				= "";
+
+	[ObservableProperty]
+	private string				_website				= "";
+
+	[ObservableProperty]
+	private string				_issuesAddress			= "";
+
+	[ObservableProperty]
+	private string				_version				= "";
+
+	#endregion
+
+	#region Construction
+
+	public AboutViewModel()
+	{
+	}
+
+	#endregion
+		
+	#region Properties
+	#endregion
+}
