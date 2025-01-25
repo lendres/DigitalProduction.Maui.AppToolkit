4745319cca64b3df14d83bdbc902487893a148b2
lendres <lanceendres@gmail.com>
2025-01-25 09:11:47 -0600

    Create FileExistsBase.cs

---
+++ src/Library/Validation/FileExistsBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalProduction.Maui.Validation;
+
+internal abstract class FileExistsBase : ValidationRuleBase<string>
+{
+public List<string>? Values { get; set; }
+}
