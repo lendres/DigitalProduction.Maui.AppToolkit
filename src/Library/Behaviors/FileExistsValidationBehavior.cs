41a195324ecfbc77fba71475d610d536c7eaf285
Ikosy <lendres@fifthrace.com>
2025-01-25 19:56:20 -0600

    Warning removal

--- src/Library/Behaviors/FileExistsValidationBehavior.cs
+++ src/Library/Behaviors/FileExistsValidationBehavior.cs
@@ -5,7 +5,7 @@ namespace DigitalProduction.Maui.Behaviors;
 /// <summary>
 /// The <see cref="FileExistsValidationBehavior"/> is a behavior that allows the user to determine if a file exists.  Additional properties handling validation are inherited from <see cref="ValidationBehavior"/>.
 /// </summary>
-public class FileExistsValidationBehavior : ErrorReportableValidationBehavior<string, PathErrorType>
+public partial class FileExistsValidationBehavior : ErrorReportableValidationBehavior<string, PathErrorType>
 {
 	/// <inheritdoc/>
 	protected override ValueTask<bool> ValidateAsync(string? value, CancellationToken token)
