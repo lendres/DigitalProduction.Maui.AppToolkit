5cf418e04d9188d2ef68194ccb966e5c92e67fd0
Ikosy <lendres@fifthrace.com>
2025-03-03 07:23:27 -0600

    Add fonts

--- src/Demo/Program/MauiProgram.cs
+++ src/Demo/Program/MauiProgram.cs
@@ -22,6 +22,7 @@ public static class MauiProgram
 			.UseMauiCommunityToolkit()
 			.UseMauiCommunityToolkitMarkup()
 			.UseDigitalProductionMaui()
+			.UseDigitalProductionMauiAppToolkit()
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
