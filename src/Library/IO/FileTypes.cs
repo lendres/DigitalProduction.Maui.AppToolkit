b90c98b224aa0cfeaa03a1a1eabf95e62a573aec
Ikosy <lendres@fifthrace.com>
2025-03-22 09:43:01 -0500

    Update FileTypes.cs

--- src/Library/IO/FileTypes.cs
+++ src/Library/IO/FileTypes.cs
@@ -9,38 +9,10 @@ public static class FileTypes
 		{
 			return new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>
 			{
-				{
-					DevicePlatform.iOS, new[]
-					{
-						"public.comma-separated-values-text",
-						"public.plain-text",
-						"public.text"
-					}
-				},
-				{
-					DevicePlatform.macOS, new[]
-					{
-						"public.comma-separated-values-text",
-						"public.plain-text",
-						"public.text"
-					}
-				},
-				{
-					DevicePlatform.Android, new[]
-					{
-						"text/csv",
-						"text/plain"
-					}
-				},
-				{
-					DevicePlatform.WinUI, new[]
-					{
-						".csv",
-						".txt",
-						".text"
-						//"*/*"
-					}
-				},
+				{ DevicePlatform.iOS, new[] { "public.comma-separated-values-text", "public.plain-text", "public.text" } },
+				{ DevicePlatform.macOS, new[] { "public.comma-separated-values-text", "public.plain-text", "public.text" } },
+				{ DevicePlatform.Android, new[] { "text/csv", "text/plain" } },
+				{ DevicePlatform.WinUI, new[] { ".csv", ".txt", ".text" } },
 			});
 		}
 	}
@@ -52,33 +24,10 @@ public static class FileTypes
 		{
 			return new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>
 			{
-				{
-					DevicePlatform.iOS, new[]
-					{
-						"com.microsoft.word.doc",
-						"org.openxmlformats.wordprocessingml.document"
-					}
-				},
-				{
-					DevicePlatform.macOS, new[]
-					{
-						"com.microsoft.word.doc",
-						"org.openxmlformats.wordprocessingml.document"
-					}
-				},
-				{
-					DevicePlatform.Android, new[]
-					{
-						"application/msword",
-						"application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-					}
-				},
-				{
-					DevicePlatform.WinUI, new[]
-					{
-						"doc","docx",
-					}
-				},
+				{ DevicePlatform.iOS, new[] { "com.microsoft.word.doc", "org.openxmlformats.wordprocessingml.document" } },
+				{ DevicePlatform.macOS, new[] { "com.microsoft.word.doc", "org.openxmlformats.wordprocessingml.document" } },
+				{ DevicePlatform.Android, new[] { "application/msword", "application/vnd.openxmlformats-officedocument.wordprocessingml.document" } },
+				{ DevicePlatform.WinUI, new[] { "doc","docx", } },
 			});
 		}
 	}
@@ -90,38 +39,10 @@ public static class FileTypes
 		{
 			return new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>
 			{
-				{
-					DevicePlatform.iOS, new[]
-					{
-						"public.xml",
-						"public.plain-text",
-						"public.text"
-					}
-				},
-				{
-					DevicePlatform.macOS, new[]
-					{
-						"public.xml",
-						"public.plain-text",
-						"public.text"
-					}
-				},
-				{
-					DevicePlatform.Android, new[]
-					{
-						"text/xml",
-						"text/plain"
-					}
-				},
-				{
-					DevicePlatform.WinUI, new[]
-					{
-						".xml",
-						".txt",
-						".text"
-						//"*/*"
-					}
-				},
+				{ DevicePlatform.iOS, new[] { "public.xml", "public.plain-text", "public.text" } },
+				{ DevicePlatform.macOS, new[] { "public.xml", "public.plain-text", "public.text" } },
+				{ DevicePlatform.Android, new[] { "text/xml", "text/plain" } },
+				{ DevicePlatform.WinUI, new[] { ".xml", ".txt", ".text" } },
 			});
 		}
 	}
@@ -133,41 +54,10 @@ public static class FileTypes
 		{
 			return new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>
 			{
-				{
-					DevicePlatform.iOS, new[]
-					{
-						"public.xslt",
-						"public.xsl",
-						"public.plain-text",
-						"public.text"
-					}
-				},
-				{
-					DevicePlatform.macOS, new[]
-					{
-						"public.xslt",
-						"public.xsl",
-						"public.plain-text",
-						"public.text"
-					}
-				},
-				{
-					DevicePlatform.Android, new[]
-					{
-						"text/xslt",
-						"text/xsl",
-						"text/plain"
-					}
-				},
-				{
-					DevicePlatform.WinUI, new[]
-					{
-						".xslt",
-						".xsl",
-						".txt",
-						".text"
-					}
-				},
+				{ DevicePlatform.iOS, new[] { "public.xslt", "public.xsl", "public.plain-text", "public.text" } },
+				{ DevicePlatform.macOS, new[] { "public.xslt", "public.xsl", "public.plain-text", "public.text" } },
+				{ DevicePlatform.Android, new[] { "text/xslt", "text/xsl", "text/plain" } }, 
+				{ DevicePlatform.WinUI, new[] { ".xslt", ".xsl", ".txt", ".text" } },
 			});
 		}
 	}
