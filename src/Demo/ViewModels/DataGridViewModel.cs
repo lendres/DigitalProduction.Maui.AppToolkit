080d69c22050b7ea63b3cf527fc61a82c1144756
Lance A. Endres <lendres@fifthrace.com>
2025-08-06 05:22:50 -0500

    Fix DataGrid styles page

--- src/Demo/ViewModels/DataGridViewModel.cs
+++ src/Demo/ViewModels/DataGridViewModel.cs
@@ -25,11 +25,11 @@ public partial class DataGridViewModel : DataGridBaseViewModel<Person>
 	#region Methods and Commands
 
 	[RelayCommand]
-	private void AddPeople()
+	public void AddPeople()
 	{
 		System.Diagnostics.Debug.Assert(Items != null);
 		Items.Add(new Person() { FirstName = "Jane",	LastName = "Doe",	Age = 30 });
-		Items.Add(new Person() { FirstName = "John",	LastName = "Doe",	Age = 31	});
+		Items.Add(new Person() { FirstName = "John",	LastName = "Doe",	Age = 31 });
 		Items.Add(new Person() { FirstName = "Jim",		LastName = "Doe",	Age = 6 });
 		Items.Add(new Person() { FirstName = "Jessie",	LastName = "Doe",	Age = 8 });
 	}
