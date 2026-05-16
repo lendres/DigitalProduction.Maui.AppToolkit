e92c269248ca30b49f72399bd82c1d5cedb99250
Lance A. Endres <lanceendres@gmail.com>
2026-05-16 07:23:07 -0500

    Add conditional validation

---
+++ src/Library/Validation/ConditionalFileExistsRule.cs
@@ -0,0 +1,16 @@
+﻿namespace DigitalProduction.Maui.Validation;
+
+/// <summary>
+/// File must exist validation rule.
+/// </summary>
+/// <remarks>
+/// Based on:
+/// https://github.com/dotnet-architecture/eshop-mobile-client
+/// Copyright (c) 2020 .NET Application Architecture - Reference Apps
+/// </remarks>
+public class ConditionalFileExistsRule : FileExistsBase, IConditionalRule
+{
+	public Func<bool> IsRequired { get; set; } = () => true;
+
+	public override bool Check(string? value) => IsRequired() ? FileExists(value) : true;
+}
---
+++ src/Library/Validation/IConditionalRule.cs
@@ -0,0 +1,14 @@
+﻿namespace DigitalProduction.Maui.Validation;
+
+/// <summary>
+/// File must exist validation rule.
+/// </summary>
+/// <remarks>
+/// Based on:
+/// https://github.com/dotnet-architecture/eshop-mobile-client
+/// Copyright (c) 2020 .NET Application Architecture - Reference Apps
+/// </remarks>
+public interface IConditionalRule
+{
+	Func<bool> IsRequired { get; set; }
+}
