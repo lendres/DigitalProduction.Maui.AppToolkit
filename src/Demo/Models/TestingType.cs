466e464e729f715a1c3a73025fc37b52454772d8
Lance A. Endres <lanceendres@gmail.com>
2026-05-21 17:50:21 -0500

    add example convert page

---
+++ src/Demo/Models/TestingType.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+
+namespace DigitalProduction.Demo.Enums;
+
+/// <summary>
+/// Add summary here.
+/// 
+/// The "Description" attribute can be accessed using Reflection to get a string representing the enumeration type.
+/// 
+/// The "Length" enumeration can be used in loops as a convenient way of terminating a loop that does not have to be changed if
+/// the number of items in the enumeration changes.  The "Length" enumeration must be the last item.
+/// for (int i = 0; i &lt; (int)EnumType.Length; i++) {...}
+/// </summary>
+public enum TestingType
+{
+	/// <summary>Type 1.</summary>
+	[Description("Enum Type 1")]
+	Type1,
+
+	/// <summary>Type 2.</summary>
+	[Description("Enum Type 2")]
+	Type2,
+
+	/// <summary>Type 3.</summary>
+	[Description("Enum Type 3")]
+	Type3
+
+} // End enum.
